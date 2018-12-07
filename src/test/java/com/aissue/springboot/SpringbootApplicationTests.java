14a10e1effe491d7bf7fe578c72f6d7999a938b3
mengtian <mengtian@yscredit.com>
2018-12-07 18:14:29 +0800

    redis相关补充

--- src/test/java/com/aissue/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/aissue/springboot/SpringbootApplicationTests.java
@@ -1,16 +1,31 @@
 package com.aissue.springboot;
 
+import com.aissue.springboot.utils.RedisUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
 
+    @Autowired
+    private RedisUtils redisUtils;
+
     @Test
     public void contextLoads() {
+        Set<String> set = new HashSet<>();
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        set.add("d");
+
+        redisUtils.sSet("testSet",set);
     }
 
 }
